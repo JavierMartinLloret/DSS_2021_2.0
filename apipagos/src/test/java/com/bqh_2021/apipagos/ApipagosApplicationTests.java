ce3b6232326f75d99ea64a849661f3d7a88204d6
juancathree <juancarloslucenamonje@gmail.com>
2021-06-13 12:24:40 +0100

    api pagos

---
+++ apipagos/src/test/java/com/bqh_2021/apipagos/ApipagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bqh_2021.apipagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
